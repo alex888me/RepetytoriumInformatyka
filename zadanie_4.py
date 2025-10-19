633afb2d3cf6849c7064d6053f4f8950aef4257b
alex8 <alex888me@gmail.com>
2025-10-19 15:32:50 +0200

    Zadanie 4

--- zadanie_4.py
+++ zadanie_4.py
@@ -26,3 +26,65 @@ Napisz algorytm w postaci pseudokodu lub w wybranym języku programowania,
 który powyższą funkcję rekurencyjną zastąpi iteracją.
 Podpowiedź: Metoda szybkiego potęgowania iteracyjnego jest nazywana również potęgowaniem binarnym.
 """
+
+
+# liczba_wywolan = 0
+
+
+def obliczenie_wylowan(x, n) -> int:
+    liczba_wywolan = 0
+
+    def oblicz(x, n):
+        nonlocal liczba_wywolan
+        liczba_wywolan += 1
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+
+        if n % 2 == 0:
+            temp = oblicz(x, n // 2)
+            return temp * temp
+        else:
+            temp = oblicz(x, (n - 1) // 2)
+            return x * temp * temp
+
+    oblicz(x, n)
+    return liczba_wywolan
+
+
+def oblicz_iteracja(x, n):
+    if n == 0:
+        return 1
+    if n == 1:
+        return x
+
+    temp = 1
+    for i in range(n):
+        temp *= x
+
+    return temp
+
+
+def oblicz_iteracja_2(x, n):
+    w = 1
+    while n > 0:
+        if n % 2 == 1:
+            w *= x
+        x *= x
+        n //= 2
+    return w
+
+
+if __name__ == '__main__':
+    podane_wartosci = {
+        (2, 10): 4,
+        (7, 13): 4,
+        (4, 16): 5,
+        (3, 35): 6,
+    }
+
+    for k, v in podane_wartosci.items():
+        assert obliczenie_wylowan(k[0], k[1]) == v
+    assert oblicz_iteracja(2, 10) == 1024
+    assert oblicz_iteracja_2(2, 10) == 1024
